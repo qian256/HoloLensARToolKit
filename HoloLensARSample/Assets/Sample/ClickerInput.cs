33c9d50fb3ff6dce38e63ea887d9ae83d7d30bec
Long Qian <lqian8@jhu.edu>
2017-01-19 15:54:49 -0500

    add sample Unity3D HoloLens project

---
+++ HoloLensARSample/Assets/Sample/ClickerInput.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.VR.WSA.Input;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// The ClickerInput class recognize the clicker event of HoloLens
+/// clicker, the event callback can be configured in Unity Inspector.
+///
+/// Author:     Long Qian
+/// Email:      lqian8@jhu.edu
+/// </summary>
+public class ClickerInput : MonoBehaviour {
+    
+    private string TAG = "ClickerInput";
+
+    [System.Serializable]
+    public class EventClickerClick : UnityEvent { };
+
+    public EventClickerClick eventClickerClick;
+
+    public static ClickerInput Instance { get; private set; }
+    GestureRecognizer recognizer;
+
+    // Use this for initialization
+    void Awake() {
+        Instance = this;
+
+        // Set up a GestureRecognizer to detect Select gestures.
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += (source, tapCount, ray) => {
+            eventClickerClick.Invoke();
+        };
+        recognizer.StartCapturingGestures();
+    }
+
+    // Use this for initialization
+    void Start() {
+        ;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ;
+    }
+}
---
+++ HoloLensARSample/Assets/Sample/FPSDisplayHUD/FpsDisplayHUD.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Calculate frame rate of Unity applications, and visualize via
+/// UnityEngine.UI.Text
+///
+/// Author:     Long Qian
+/// Email:      lqian8@jhu.edu
+/// </summary>
+public class FpsDisplayHUD : MonoBehaviour
+{
+    public Text Text;
+
+    private float deltaTime;
+
+    private void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        var msec = deltaTime * 1000.0f;
+        var fps = 1.0f / deltaTime;
+        var text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        Text.text = text;
+    }
+}
